dd62c5fe3d67f0545985e8deeabfad70e1a97c57
Michael Jones <59059883+michaeljones256@users.noreply.github.com>
2021-10-17 16:45:28 -0400

    Update index.py

--- index.py
+++ index.py
@@ -2,7 +2,6 @@ from game_character import GameCharacter
 from game import Game
 
 def main():
-    print("hello world")
     character1 = GameCharacter("Mario",27,["Fireball"],100)
     character1.displayCharacter()
 
