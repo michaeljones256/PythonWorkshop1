1d3a9761c7cc92709292e7f07e0787fb20eeb683
Michael Jones <59059883+michaeljones256@users.noreply.github.com>
2021-10-17 16:45:02 -0400

    Update game_character.py

--- game_character.py
+++ game_character.py
@@ -4,3 +4,7 @@ class GameCharacter:
         self.age = age
         self.abilites = abilites
         self.hp = hp
+    def displayCharacter(self):
+        print("Name: {}\nAge: {}\nHP: {}\nAbilties:".format(self.name, self.age, self.hp))
+        for ability in self.abilites:
+            print("- " + ability)
