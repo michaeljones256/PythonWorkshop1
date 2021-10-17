19bf0dee0cd697be6e7c73f5f568a90960b7e866
Michael Wesley Jones <mwjones@syr.edu>
2021-10-17 16:12:05 -0400

    Second3

--- Game.py
+++ Game.py
@@ -1,7 +1,7 @@
 class Game:
     def __init__ (self,name, description, characters):
         self.gameName = name
-        self.gaemDescription = description
+        self.gameDescription = description
         self.character = characters
 
     
