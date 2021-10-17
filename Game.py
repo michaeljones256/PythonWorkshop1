9e0705792e3eb0228a5cbd7d5bd475a27111ab9e
Michael Wesley Jones <mwjones@syr.edu>
2021-10-17 16:09:31 -0400

    Second

--- Game.py
+++ Game.py
@@ -1,1 +1,8 @@
-print("hello")
+class Game:
+    def __init__ (self,name, description, characters):
+        self.gameName = name
+        self.gaemDescription = description
+        self.character = characters
+
+    
+
---
+++ GameCharacter.py
@@ -0,0 +1,5 @@
+class GameCharacter:
+    def __init__ (self,name, age, abilites):
+        self.name = name
+        self.age = age
+        self.abilites = abilites
