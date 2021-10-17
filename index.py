68e490e9a6d5843149abddd0eda80a70f62b404b
Michael Jones <59059883+michaeljones256@users.noreply.github.com>
2021-10-17 16:45:19 -0400

    Update index.py

--- index.py
+++ index.py
@@ -3,6 +3,8 @@ from game import Game
 
 def main():
     print("hello world")
+    character1 = GameCharacter("Mario",27,["Fireball"],100)
+    character1.displayCharacter()
 
 
 if __name__ == "__main__":
