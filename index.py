468e4863391e45490defbdcfbb85d2e2d6f50b11
Michael Wesley Jones <mwjones@syr.edu>
2021-10-17 16:31:36 -0400

    final

--- index.py
+++ index.py
@@ -1,7 +1,11 @@
-import Game, GameCharacter
+
+
+
+from game_character import GameCharacter
+from game import Game
 
 def main():
-    print("Hello World!")
+    print("hello world")
 
 if __name__ == "__main__":
     main()
